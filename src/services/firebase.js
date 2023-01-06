e792f8909e583b7bbe83bb1fbd4d47ae048491ed
Ajay Raj <ajayraj@Ajays-MacBook-Pro.local>
2023-01-05 22:18:13 -0800

    adds firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,32 @@
 import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  onSnapshot,
+  query,
+  collection,
+  orderBy,
+} from "firebase/firestore";
+
 const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   // TODO: Add your Firebase configuration here
 };
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export const getMessages = (chatId, callback) => {
+  return onSnapshot(
+    query(
+      collection(db, "chats", chatId, "messages"),
+      orderBy("timestamp", "asc")
+    ),
+    (querySnapshot) => {
+      const messages = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      callback(messages);
+    }
+  );
+};
