cd683c66827f1922bd57d099599314c0b922cf1b
Ajay Raj <ajay.n.raj@gmail.com>
2023-03-31 23:50:47 -0700

    remove anon calls

--- src/components/PrankForm.tsx
+++ src/components/PrankForm.tsx
@@ -126,7 +126,7 @@ const PrankForm = ({
         padding={"10px"}
         onChange={(text) => setPrompt(text.target.value)}
       />
-      <Text fontSize="20px" padding="10px">
+      {/* <Text fontSize="20px" padding="10px">
         4. Do you want to call using your number or remain anonymous?
       </Text>
       <Checkbox
@@ -137,7 +137,7 @@ const PrankForm = ({
         }}
       >
         Remain anonymous
-      </Checkbox>
+      </Checkbox> */}
       <Box padding="10px" width="100%">
         <Center>
           <Button
